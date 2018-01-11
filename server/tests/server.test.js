0449aefb3d52719eda78b955a3ccdcf3e98e6792
root <root@localhost.localdomain>
2018-01-11 11:33:49 -0500

    Test POSt/todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -31,5 +31,22 @@ describe('POST /todos', ()=> {
 		done();
 		}).catch((e)=> done(e))
 	      });
-	})	
+	})
+	it ('should not create todo with invalid body data', (done)=>{
+
+		request(app)
+		.post('/todos')
+		.send({})
+		.expect(400)
+			.emd((err, res)=>{
+				if (err){
+					return done(err);
+				}
+				Todo.find().then((todos)=> {
+					expect(todos.length).toBe(0);
+					done();
+				}).catch((err) => (done));	
+			})
+
+	})
 })
